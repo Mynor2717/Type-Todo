36e05a4c36e885964100c5a17770d0daa63f4f27
node <ratafak15@gmail.com>
2024-03-08 20:30:39 -0500

    Typescript con react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
+import { useState } from "react";
+
+interface Todo {
+  text: string;
+  complete: boolean;
+}
+
+const initialTodos = [
+  {
+    text: "Learn React",
+    complete: false,
+  },
+  {
+    text:"learn TypeScript",
+    complete: false,
+  },
+];
+
+
+
 const App = () => {
+
+
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+
+
   return (
-    <div>App</div>
+    <div className="container">
+      <h1>TooDo</h1>
+      <form>
+        <label htmlFor="todo">
+          Todo
+          <input type="text" id="todo" placeholder="ingrese todo"/>
+          <button type="submit">Add todo</button>
+        </label>
+      </form>
+    </div>
   )
 }
 
